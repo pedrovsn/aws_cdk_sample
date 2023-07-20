3bb616553c762c9aeca880342c6ac3a4096a98ac
Pedro Sousa <pedro-vsn@hotmail.com>
2023-07-19 23:12:54 -0300

    First commit

--- aws_cdk_app_sample/aws_cdk_app_sample_stack.py
+++ aws_cdk_app_sample/aws_cdk_app_sample_stack.py
@@ -1,9 +1,12 @@
+import os
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_lambda as lambda_,
+    aws_apigateway as api_gw_
 )
 from constructs import Construct
+
+DIRNAME = os.path.dirname(__file__)
 
 class AwsCdkAppSampleStack(Stack):
 
@@ -11,9 +14,19 @@ class AwsCdkAppSampleStack(Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
+        fn = lambda_.Function(
+            self, "HelloWorldFn",
+            runtime=lambda_.Runtime.PYTHON_3_10,
+            handler="index.handler",
+            code=lambda_.Code.from_asset(os.path.join(DIRNAME, "src")),
+            environment={}
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "AwsCdkAppSampleQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        api = api_gw_.LambdaRestApi(
+            self, "HelloWorldAPI",
+            handler=fn,
+            proxy=False
+        )
+
+        hello_world = api.root.add_resource("hi")
+        hello_world.add_method("GET")
---
+++ aws_cdk_app_sample/src/index.py
@@ -0,0 +1,15 @@
+from http import HTTPStatus
+
+import json
+import os
+
+def handler(event, context):
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "body": json.dumps({
+            "Hello": "World"
+        })
+    }
